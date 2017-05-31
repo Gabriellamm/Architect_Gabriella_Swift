455089c8ab6421859bdec895896a02637fe387ba
Gabriellamm <379713491@qq.com>
2017-05-31 23:15:45 +0800

    test

---
+++ Architect_Swift/Architect_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Architect_Swift
+//
+//  Created by Gabriella on 2017/5/27.
+//  Copyright © 2017年 Gabriella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
